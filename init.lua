b4dc972dd5b99643a6ce059fc465f0605e377769
Erik Jarem <erik.jarem@gmail.com>
2024-06-09 14:46:24 +0200

    Initial nvim setup

---
+++ init.lua
@@ -0,0 +1,65 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.6',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  { "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  }
+}
+
+local opts = {}
+
+-- LAZY
+require("lazy").setup(plugins, opts)
+
+-- OPTIONS
+require("vim-options")
+
+
+--TELESCOPE
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.find_files, {})
+
+
+--CATPPUCCIN
+require("catppuccin").setup()
+
+vim.cmd.colorscheme "catppuccin"
