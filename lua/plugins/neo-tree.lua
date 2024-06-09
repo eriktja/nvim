08515df8e901480b2b683612f274ca7082f5dd32
Erik Jarem <erik.jarem@gmail.com>
2024-06-09 19:48:21 +0200

    lualine is hip, and everyone has one

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,3 @@
- -- return {
- --   'nvim-neo-tree/neo-tree.nvim',
- --   version = '*',
- --   dependencies = {
- --     'nvim-lua/plenary.nvim',
- --     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
- --     'MunifTanjim/nui.nvim',
- --   }
- -- }
-
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
