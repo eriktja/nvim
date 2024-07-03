7c9196069e9749729314b76805c820f7d48e84dc
Erik Jarem <erik.jarem@gmail.com>
2024-07-03 22:29:31 +0200

    stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,6 +15,7 @@ return {
 					"pyright",
 					"clangd",
 					"rust_analyzer",
+          "angularls",
 				},
 			})
 		end,
@@ -41,15 +42,20 @@ return {
 				capabilities = capabilities,
 				cmd = { "rustup", "run", "stable", "rust-analyzer" },
 			})
+      lspconfig.angularls.setup({
+        capabilities = capabilities,
+      })
 
 			-- KEY BINDINGS
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
       vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+      -- vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      -- vim.keymap.set("n", "]e", vim.lsp.diagnostic.goto_next, {})
+      -- vim.keymap.set("n", "[e", vim.lsp.diagnostic.goto_prev, {})
 		end,
 	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,11 +5,36 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
+
+      -- FIND FILES
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+      vim.keymap.set("n", "<leader>fr", builtin.registers, {})
+      vim.keymap.set("n", "<leader>fc", builtin.commands, {})
+      vim.keymap.set("n", "<leader>bf", builtin.current_buffer_fuzzy_find, {})
+      vim.keymap.set("n", "<leader>fw", builtin.current_buffer_tags, {})
+
+      -- LSP
+      vim.keymap.set("n", "<leader>gr", builtin.lsp_references, {})
+      vim.keymap.set("n", "<leader>gd", builtin.lsp_definitions, {})
+      vim.keymap.set("n", "<leader>gi", builtin.lsp_implementations, {})
+      vim.keymap.set("n", "<leader>qf", builtin.quickfix, {})
+      vim.keymap.set("n", "<leader>lf", builtin.lsp_document_symbols, {})
+      vim.keymap.set("n", "<leader>lw", builtin.lsp_workspace_symbols, {})
+      -- vim.keymap.set("n", "<leader>sd", builtin.lsp_document_diagnostics, {})
+      -- vim.keymap.set("n", "<leader>sD", builtin.lsp_workspace_diagnostics, {})
+
+
+      ---- GIT
+      vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
+      vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
+      vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
+      vim.keymap.set("n", "<leader>gl", builtin.git_bcommits, {})
+      vim.keymap.set("n", "<leader>gt", builtin.git_stash, {})
+
     end,
   },
   {
